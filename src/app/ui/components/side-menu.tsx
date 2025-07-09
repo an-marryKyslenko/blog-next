d9a50d19afbcaf44a5958caaf6a1b6e14a647c19
an-marryKyslenko <olih.a@icloud.com>
2025-07-09 22:19:03 +0300

    fix lint

--- src/app/ui/components/side-menu.tsx
+++ src/app/ui/components/side-menu.tsx
@@ -1,20 +1,6 @@
 import React from 'react'
 import NavLinks from './nav-links'
 
-const links = [
-	{
-		name: 'About',
-		path: '/blog/about',
-	},
-	{
-		name: 'Contact',
-		path: '/blog/contact',
-	},
-	{
-		name: 'Posts',
-		path: '/blog/posts',
-	},
-]
 const SideNav = () => {
 	return (
 		<div className="flex h-full flex-col px-3 py-4 md:px-2">
